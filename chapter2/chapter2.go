9bfb7ef0f43cf084c1c427676ca68e555bb20aff
shigematsu <shigematsu.m@tam-tam.co.jp>
2019-12-23 01:17:00 +0900

    update: gihyo.jp の Go 入門の第2章 switch 部分まで写経

--- chapter2/chapter2.go
+++ chapter2/chapter2.go
@@ -39,6 +39,13 @@ func Exec() {
 	const hello string = "hello"
 	f.Println(hello)
 
+	ifSyntax()
+	forSyntax()
+	switchSyntax()
+
+}
+
+func ifSyntax() {
 	// if 構文
 	// 三項演算子は使えない
 	// {} の省略記法はコンパイルエラーになる
@@ -50,7 +57,9 @@ func Exec() {
 	} else {
 		f.Println("a と b は等しい")
 	}
+}
 
+func forSyntax() {
 	// for 構文
 	// while や do/while は存在しない、全て for を利用する
 	// 無限ループは for {...} で実行できる
@@ -73,6 +82,49 @@ func Exec() {
 		}
 
 		// 奇数のみ表示
+		f.Println(n)
+	}
+}
+
+func switchSyntax() {
+	// switch 構文
+	n := 10
+	switch n {
+	case 15:
+		f.Println("FizzBuzz")
+	case 5, 10:
+		f.Println("Buzz")
+	case 3, 6, 9:
+		f.Println("Fizz")
+	default:
+		f.Println(n)
+	}
+
+	// Go の Switch は case に break を記述しなくても次の case に進まない
+	// 次の case に進めたい場合は fallthrough を記述する
+	n = 3
+	switch n {
+	case 3:
+		n = n - 1
+		fallthrough
+	case 2:
+		n = n - 1
+		fallthrough
+	case 1:
+		n = n - 1
+		f.Println(n)
+	}
+
+	// 評価式でも switch を利用できる
+	n = 10
+	switch {
+	case n%15 == 0:
+		f.Println("FizzBuzz")
+	case n%5 == 0:
+		f.Println("Buzz")
+	case n%3 == 0:
+		f.Println("Fizz")
+	default:
 		f.Println(n)
 	}
 }
